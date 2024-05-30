056ac20051faeee10be7ccd8db27b08b2983a276
Mixalis Karvelas <makarvelas@athtech.gr>
2024-05-30 04:23:20 +0300

    php code

---
+++ Info.php
@@ -0,0 +1,28 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Process the login info here
+    // ...
+}
+?>
+<?php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Process the login info here
+    // ...
+
+    // Store account information in session variables
+    $_SESSION['username'] = $username;
+    $_SESSION['password'] = $password;
+
+    // Redirect to a logged-in page
+    header('Location: logged-in.php');
+    exit;
+}
+?>
